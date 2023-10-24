274b36c49a6f3ca06b50005d849e5ff9c539b98e
Mumina033 <mumina.mohamed033@gmail.com>
2023-10-24 19:57:53 -0400

    Added JS and updated the link color

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,33 @@
+function updatTime() {
+  let phoenixElement = document.querySelector("#phoenix");
+  let phoenixDateElement = phoenixElement.querySelector(".date");
+  let phoenixTimeElement = phoenixElement.querySelector(".time");
+  let phoenixTime = moment().tz("America/Phoenix");
 
+  phoenixDateElement.innerHTML = phoenixTime.format("MMMM Do YYYY");
+  phoenixTimeElement.innerHTML = phoenixTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let denverElement = document.querySelector("#denver");
+  let denverDateElement = denverElement.querySelector(".date");
+  let denverTimeElement = denverElement.querySelector(".time");
+  let denverTime = moment().tz("America/denver");
+
+  denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
+  denverTimeElement.innerHTML = denverTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updatTime;
+setInterval(updatTime, 1000);
