2de2eb45ba9028dae1c7e94419d2ab12eae70c57
Mumina033 <mumina.mohamed033@gmail.com>
2023-10-25 00:24:11 -0400

    updated the select feature and added an All cities link

--- javascript/script.js
+++ javascript/script.js
@@ -30,8 +30,43 @@ function updateTime() {
   );
 }
 
+function updateTime() {
+  let phoenixElement = document.querySelector("#phoenix");
+  let phoenixDateElement = phoenixElement.querySelector(".date");
+  let phoenixTimeElement = phoenixElement.querySelector(".time");
+  let phoenixTime = moment().tz("America/Phoenix");
+
+  phoenixDateElement.innerHTML = phoenixTime.format("MMMM Do YYYY");
+  phoenixTimeElement.innerHTML = phoenixTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let denverElement = document.querySelector("#denver");
+  let denverDateElement = denverElement.querySelector(".date");
+  let denverTimeElement = denverElement.querySelector(".time");
+  let denverTime = moment().tz("America/Denver");
+
+  denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
+  denverTimeElement.innerHTML = denverTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    currentTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -45,6 +80,7 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+  <a href="index.html">All cities</a>
   `;
 }
 
