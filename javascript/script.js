387e87547b4f4400be71c0a65e1cf9818c730996
Mumina033 <mumina.mohamed033@gmail.com>
2023-10-24 22:40:46 -0400

    Added the functionality for the select

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-function updatTime() {
+function updateTime() {
   let phoenixElement = document.querySelector("#phoenix");
   let phoenixDateElement = phoenixElement.querySelector(".date");
   let phoenixTimeElement = phoenixElement.querySelector(".time");
@@ -22,12 +22,34 @@ function updatTime() {
   let denverElement = document.querySelector("#denver");
   let denverDateElement = denverElement.querySelector(".date");
   let denverTimeElement = denverElement.querySelector(".time");
-  let denverTime = moment().tz("America/denver");
+  let denverTime = moment().tz("America/Denver");
 
   denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
   denverTimeElement.innerHTML = denverTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
 }
-updatTime;
-setInterval(updatTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
