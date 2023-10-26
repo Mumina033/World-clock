dfeb5e3fa5019dca116d7c1f56cfb3d73c3cd72a
Mumina033 <mumina.mohamed033@gmail.com>
2023-10-26 16:04:18 -0400

    Fixed an issue with the date being in the wrong place

--- javascript/script.js
+++ javascript/script.js
@@ -30,38 +30,6 @@ function updateTime() {
   );
 }
 
-function updateTime() {
-  let phoenixElement = document.querySelector("#phoenix");
-  let phoenixDateElement = phoenixElement.querySelector(".date");
-  let phoenixTimeElement = phoenixElement.querySelector(".time");
-  let phoenixTime = moment().tz("America/Phoenix");
-
-  phoenixDateElement.innerHTML = phoenixTime.format("MMMM Do YYYY");
-  phoenixTimeElement.innerHTML = phoenixTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
-  let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".date");
-  let londonTimeElement = londonElement.querySelector(".time");
-  let londonTime = moment().tz("Europe/London");
-
-  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
-  let denverElement = document.querySelector("#denver");
-  let denverDateElement = denverElement.querySelector(".date");
-  let denverTimeElement = denverElement.querySelector(".time");
-  let denverTime = moment().tz("America/Denver");
-
-  denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
-  denverTimeElement.innerHTML = denverTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-}
-
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
@@ -70,8 +38,7 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city">
+  citiesElement.innerHTML = `<div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
